e9b9dc7b69c1b76d488d28c55b94903c4951b19d
Kemps Medeiros <kemps.medeiros@kabum.com.br>
2023-10-12 15:01:28 -0300

    feat: first commit

---
+++ src/main/java/com/desafio/spring/api/desafiospringapidio/DesafioSpringApiDioApplication.java
@@ -0,0 +1,13 @@
+package com.desafio.spring.api.desafiospringapidio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioSpringApiDioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioSpringApiDioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/desafio/spring/api/desafiospringapidio/DesafioSpringApiDioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio.spring.api.desafiospringapidio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSpringApiDioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
